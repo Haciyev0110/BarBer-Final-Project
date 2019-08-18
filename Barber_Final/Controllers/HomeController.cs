0a907791ba152311146150d0ee3c25c7259eb9f6
Haciyev0110 <panahrh@code.edu.az>
2019-08-18 14:38:54 +0400

    Code Academy Asp.Net Final Project

---
+++ Barber_Final/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Barber_Final.Models;
+using Barber_Final.Vm;
+
+namespace Barber_Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        BarberShOWEntities db = new BarberShOWEntities();
+        
+
+        public ActionResult Index()
+        {
+            ViewModel mnv = new ViewModel();
+
+            mnv.mainSlider = db.MainSlider.ToList();
+            mnv.Aboutus = db.AboutCenter.First();
+            mnv.Detalis = db.About_details.First();
+            mnv.House = db.Working_House.ToList();
+            mnv.Servicesss = db.Our_Services.First();
+            mnv.Cartinca = db.Our_Cartinca.ToList();
+            mnv.Apoints = db.Appoint.First();
+
+
+            return View(mnv);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Barber_Final/Vm/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Barber_Final.Models;
+
+namespace Barber_Final.Vm
+{
+    public class ViewModel
+    {
+        public List<MainSlider> mainSlider { get; set; }
+       public AboutCenter Aboutus { get;set; }
+        public About_details Detalis { get; set; }
+        public   List<Working_House>  House { get; set; }
+        public Our_Services Servicesss { get; set; }
+        public   List<Our_Cartinca>  Cartinca { get; set; }
+        public Appoint Apoints { get; set; }
+    }
+}
